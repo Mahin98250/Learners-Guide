535805a3ef4efc8fa0fc6f4ab6e4368c8c216a22
Mahin98250 <patelmahin140@gmail.com>
2026-09-01 05:24:57 +0530

    fix: refresh service worker after mobile layout repair

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "learners-guide-v23";
+const CACHE = "learners-guide-v24";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./file_00000000451c82118020d2baea54f76b.png"];
 const APP_SCOPE = self.registration?.scope || self.location.href;
 const STATIC_DESTINATIONS = new Set(["script", "style", "image", "font"]);
